e6c66ecd40ea1a4099a5b643093d4161d2123713
RuslanM <rmukashev@mail.ru>
2019-12-05 11:28:23 +0600

    last update

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ import time
 def test_addbutton_shoud_be_on_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(2)
+    time.sleep(30)
     add_button = browser.find_element_by_xpath("//button[contains(@class, 'btn btn-lg btn-primary btn-add-to-basket')]")
     button_text = add_button.text
     assert button_text != '', 'кнопка не найдена'
